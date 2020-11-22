59a4d56d01cba3b63d3edcbfb017aadfd449f0fe
Moataz-AbdElKhalek <Moataz-AbdElKhalek@users.noreply.github.com>
2020-11-22 12:59:00 -0600

    Adding ANN Classification Model

--- concrete_ml.py
+++ concrete_ml.py
@@ -23,3 +23,17 @@ def NN_Regression(X,y, predict_sample=X[0]):
   prediction = NN_Regressor.predict(predict_sample)
 
   return score,prediction
+
+def NN_Classification(X,y, predict_sample=X[0]):
+  # Divide the input dataset:
+  X_train, X_test, y_train, y_test = dataset_divide(X,y)
+
+  # Classification Model Training:
+  model = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(100,100,100), activation='relu',random_state=1, max_iter=1000)
+  NN_Classifier = model.fit(X_train, y_train)
+  score = NN_Classifier.score(X_test, y_test)
+  prediction = NN_Classifier.predict(predict_sample)
+
+  return score,prediction
+
+
