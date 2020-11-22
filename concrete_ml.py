75438d3bca82ba31043ca791ed261508cc39bbbb
Moataz-AbdElKhalek <Moataz-AbdElKhalek@users.noreply.github.com>
2020-11-22 12:58:04 -0600

    Adding ANN Regression Model

--- concrete_ml.py
+++ concrete_ml.py
@@ -11,4 +11,15 @@ def dataset_divide(X, y):
   X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y)
   return X_train, X_test, y_train, y_test
 
+# ANN Regression Model:
+def NN_Regression(X,y, predict_sample=X[0]):
+  # Divide the input dataset:
+  X_train, X_test, y_train, y_test = dataset_divide(X,y)
 
+  # Regression Model Training:
+  model = sklearn.neural_network.MLPRegressor(hidden_layer_sizes=(100,100,100), activation='relu',random_state=1, max_iter=1000)
+  NN_Regressor = model.fit(X_train, y_train)
+  score = NN_Regressor.score(X_test, y_test)
+  prediction = NN_Regressor.predict(predict_sample)
+
+  return score,prediction
